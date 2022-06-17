8035f5b6152970cf513fa0e33f50f09cf5ccfbb7
Jose Miguel Gomes <jose_miguel_gomes@hotmail.com>
2022-06-17 12:36:46 +0100

    React Calculator

---
+++ src/components/Button.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import './Button.css';
+
+// eslint-disable-next-line import/no-anonymous-default-export
+export default props => {
+    let classes = 'button '
+    classes += props.operation ? 'operation' : ''
+    classes += props.double ? 'double' : ''
+    classes += props.triple ? 'triple' : ''
+
+    return (
+      <button
+        onClick={e => props.click &&  props.click(props.label)}
+        className={classes}>
+        {props.label}
+      </button>
+    )
+};
